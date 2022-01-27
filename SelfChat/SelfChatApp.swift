9ebe1c575b7449844fe8bf5d04b6b5e40886a45f
sunanmau5 <sunan.regi@gmail.com>
2022-01-27 23:58:35 +0100

    Initial Commit

---
+++ SelfChat/SelfChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfChatApp.swift
+//  SelfChat
+//
+//  Created by MBA on 27.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SelfChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
